7db7e5e4d603c6beb26149210efe43bf56af207d
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2020-05-10 20:39:54 +0200

    Update DerSpiegelScraper.py

--- DerSpiegelScraper.py
+++ DerSpiegelScraper.py
@@ -4,43 +4,53 @@ Created on Sun May 10 09:35:53 2020
 
 @author: Amr.Khalil
 """
+# Import libraries
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm
 import os
 
+# The website categories
 category = ['politik', 'ausland','panorama', 'sport', 'wirtschaft', 'netzwelt', 'wissenschaft', 'kultur', 'geschichte']
-
+# Create a dictionary for articles
 my_dict={}
-
+# Number of pages that will be scraped for every categotry
 MAX_PAGES = 501
-
+# Indexing the articles
 article_num = 1
+
 for cat in category:
     for page in tqdm(range(1,MAX_PAGES),  desc='{:>15}'.format(cat)): 
         url = "https://www.spiegel.de/{}/p{}".format(cat, page)
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
 
-        articles = soup.select('article')
+        articles = soup.select('article') # select all articles
         for a in articles:
+            # Select the articles' titles from attribute 'aria-label'
             my_dict[article_num]={"title":a.get('aria-label')}
+            
+            # Select the articles' summary from class 'leading-loose'
             my_dict[article_num].update({"summary":"".join([x.text.strip()
                                                             for x in a.select('.leading-loose')])
                                         })
+            # Select the articles' date from 'footer'
             my_dict[article_num].update({"date":"".join([x.text.strip()
                                                             for x in a.select('footer')])
                                         })
+            # Add the category
             my_dict[article_num].update({"categoty":cat})
+            
+            # index' Increment  
             article_num +=1
 
 if 'data' not in os.listdir():          
-    #create a folder
+    # Create a folder
     !mkdir data
     #Convert my_dict to DataFrame
     df = pd.DataFrame.from_dict(data=my_dict, orient='index')
     # Drop duplicate articles
     df2 = df.drop_duplicates(subset='title', keep='first')
     # Save as csv as utf-16 due to spicial German charachters
-    df.to_csv('data/DerSpiegel.csv', index=False, encoding = 'utf-16')
+    df2.to_csv('data/DerSpiegel.csv', index=False, encoding = 'utf-16')
